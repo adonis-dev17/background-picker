0747a3c63243c48416d898f10d983c4171665707
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-05 03:42:11 -0500

    Add orange and red background options.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
 document.getElementById('yellow').onclick = partyYellow
+document.getElementById('orange').onclick = partyOrange
+document.getElementById('red').onclick = partyRed
 
 function partyPurple() {
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
@@ -20,5 +22,13 @@ function partyBlue() {
 
 function partyYellow() {
   document.querySelector('body').style.backgroundColor = 'rgb(255, 255, 0)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyOrange() {
+  document.querySelector('body').style.backgroundColor = 'rgb(227, 151, 10)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'rgb(220, 9, 9)'
   document.querySelector('body').style.color = 'white'
 }
